e0158914fb6e900ed4fbb76740881e382437dd60
桑原雅史 <brucemclarenp4@gmail.com>
2021-04-07 16:23:27 +0900

    商品出品機能、単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :description
+    validates :price, numericality: {with: /\A[0-9]+\z/, message: "is invalid."}
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+  end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name             {'タイトル'}
+    description           {'説明文'}
+    category_id           {2}
+    status_id             {2}
+    shipping_area_id      {2}
+    shipping_charge_id    {2}
+    days_to_ship          {2}
+    price                 {1000}
   end
 end
