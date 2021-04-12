5141226fed2788ab979e0697be0e87869c4ba7c2
桑原雅史 <brucemclarenp4@gmail.com>
2021-04-12 12:15:46 +0900

    before_actionにdestroy追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only:[:edit, :update, :show]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -36,7 +36,6 @@ class ItemsController < ApplicationController
   
   def destroy
     item = Item.find(params[:id])
-    item.destroy
     if item.destroy
       redirect_to root_path
     end
