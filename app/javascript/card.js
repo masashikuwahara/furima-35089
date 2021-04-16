e3e5dc4f11d0c5f551fd75641df236bddea1242c
桑原雅史 <brucemclarenp4@gmail.com>
2021-04-16 12:50:37 +0900

    indexにsoldout!を表示

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,10 +8,10 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("purchase_address[number]"),
-      cvc: formData.get("purchase_address[cvc]"),
-      exp_month: formData.get("purchase_address[exp_month]"),
-      exp_year: `20${formData.get("purchase_address[exp_year]")}`,
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
     };
 
     Payjp.createToken(card, (status, response) => {
@@ -20,7 +20,9 @@ const pay = () => {
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
+
       }
+
       document.getElementById("card-number").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
       document.getElementById("card-exp-month").removeAttribute("name");
